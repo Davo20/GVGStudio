c894b08ff1cb26adb78249a7c3961f682848bf3e
Davo20 <dav507805@gmail.com>
2024-01-26 15:57:40 +0400

    GVG Studio

--- src/Component/Nav/Burger.js
+++ src/Component/Nav/Burger.js
@@ -10,62 +10,63 @@ export default function Burger({ languageClick, language, selectLanguage, menuBu
                 {selectLanguage[language].map((lang) => {
                     return <ul key={Math.random()}>
                         <div className="menuLink">
-                            <Link to="/" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.home}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                            <Link to="/about" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/about" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.about}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                            <Link to="/services" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/services" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.service}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                            <Link to="/portfolio" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/portfolio" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.portfolio}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                            <Link to="/clients" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/clients" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.clients}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                            <Link to="/contact" onClick={()=>setMenuBurger(false)}>
+                            <Link to="/contact" onClick={() => setMenuBurger(false)}>
                                 <li>{lang.contact}</li>
                             </Link>
                         </div>
                         <div className="menuLink">
-                        <li>
-                            <div className="selectLanguage">
-                              
-                                <Select
-                                    // labelInValue
-                                    defaultValue={{ value: "English", label: "EN" }}
-                                    style={{ width: 60, height: 20 }}
-                                    listItemHeight={30}
-                                    onChange={languageClick}
-                                    options={[
-                                        {
-                                            value: "English",
-                                            label: "EN"
-                                        },
-                                        {
-                                            value: "Armenian",
-                                            label: "AM"
-                                        },
-                                        {
-                                            value: "Russian",
-                                            label: "RU"
-                                        },
-                                    ]}
-                                />
-                            </div>
-                        </li>
+                            <li>
+                                <div className="selectLanguage">
+
+                                    <Select
+                                        // labelInValue
+                                        value={{ value: language }}
+                                        defaultValue={{ value: "Armenian" }}
+                                        style={{ width: 60, height: 20 }}
+                                        listItemHeight={30}
+                                        onChange={languageClick}
+                                        options={[
+                                            {
+                                                value: "English",
+                                                label: "EN"
+                                            },
+                                            {
+                                                value: "Armenian",
+                                                label: "AM"
+                                            },
+                                            {
+                                                value: "Russian",
+                                                label: "RU"
+                                            },
+                                        ]}
+                                    />
+                                </div>
+                            </li>
                         </div>
                     </ul>
                 })}
